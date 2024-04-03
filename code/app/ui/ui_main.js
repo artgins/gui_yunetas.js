8bc64a8bccbbec8f7263f43ececa881aa352aa63
rmm <rosa.martinez@artgins.com>
2024-04-03 13:25:22 +0200

    fix top don't run in cordova

--- code/app/ui/ui_main.js
+++ code/app/ui/ui_main.js
@@ -218,11 +218,13 @@
          *  Menu "app"
          */
         jQuery("#top-left-layer").html(`
-            <button class="button without-border">
-                <span class="icon is-medium is-responsive">
-                    <svg id="icon-state-yuneta" viewBox="0 0 448 512"><path fill="${self.config.color_yuneta_disconnected}" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
-                </span>
-            </button>
+            <div class="buttons">
+                <button class="button without-border">
+                    <span class="icon is-medium">
+                        <svg id="icon-state-yuneta" viewBox="0 0 448 512"><path fill="${self.config.color_yuneta_disconnected}" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+                    </span>
+                </button>
+            </div>
         `);
 
         /*
@@ -230,16 +232,22 @@
          *  change theme, username, account menu
          */
         jQuery("#top-right-layer").html(`
-            ${html_change_theme()}
-            <span id="tag_username" class="tag"></span>
-            <button class="button without-border">
-                <span class="icon is-medium is-responsive">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="${self.config.color_user_logout}" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
-                </span>
-            </button>
+            <div class="is-responsive">
+                <button class="button without-border">
+                    <span class="icon">
+                        <i class="fas fa-lg fa-sun"></i>
+                    </span>
+                </button>
+                <span id="tag_username" class="tag">pepe@xxx.com</span>
+                <button class="button without-border">
+                    <span class="icon is-medium">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="${self.config.color_user_logout}" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+                    </span>
+                </button>
+            </div>
         `);
 
-        themes();
+        //themes();
 
         /*
          *  Center App Logo
@@ -249,9 +257,11 @@
             path_root
         );
         jQuery("#top-center-layer").html(`
-            <figure class="image" style="width: 80px; height: 38px;">
-                <img src="${image_file}" alt="App logo"/>
-            </figure>
+            <div class="buttons">
+                <figure class="image" style="width: 80px; height: 38px;">
+                    <img src="${image_file}" alt="App logo"/>
+                </figure>
+            </div>
         `);
 
         /*---------------------------------------*
