86db94cde7ed7135db6bc4d883978a39f11930b5
rmm <rosa.martinez@artgins.com>
2024-04-04 20:03:36 +0200

    falta ajustar los svg moon,sun

--- code/app/ui/ui_main.js
+++ code/app/ui/ui_main.js
@@ -33,146 +33,6 @@
         //////////////// Private Attributes /////////////////
     };
 
-    const colorModes = {
-        light: "light",
-        dark: "dark"
-    };
-
-    const sizes = {
-        extraSmall: "extraSmall",
-        small: "small",
-        medium: "medium",
-        large: "large"
-    };
-
-    // style={
-    //     {
-    //         backgroundColor: (colorMode === colorModes.dark && botColors?.background),
-    //         color: (colorMode === colorModes.dark && botColors?.font)
-    //     }
-
-    // const theme = useMemo(() => {
-    //     document.body.classList.remove(mode === colorModes.dark ? colorModes.light : colorModes.dark);
-    //     document.body.classList.add(mode);
-    //     return createTheme({palette: {
-    //         mode,
-    //         primary: {
-    //             main: botColors?.fontActive,
-    //           },
-    //         }})
-    // }, [botColors?.fontActive, mode]);
-
-
-// export function ColorModeProvider({children}) {
-//     const [mode, setMode] = useState(colorModes.dark);
-//     const colorMode = useMemo(() => ({
-//         toggleColorMode: () => {
-//             setMode((prevMode) => (prevMode === colorModes.light ? colorModes.dark : colorModes.light));
-//         }
-//     }), []);
-//     return (<ToggleColorModeContext.Provider value={colorMode}>
-//         <ColorModeContext.Provider value={mode}>
-//                 {children}
-//         </ColorModeContext.Provider>
-//     </ToggleColorModeContext.Provider>);
-// }
-
-// export default function ColorModeSwitch({
-//     onClick
-// }) {
-//     const colorMode = useColorModeContext()
-//     const colorModeToggle = useToggleColorModeContext();
-//     return useMemo(() => {
-//         return (<AntButton
-//             text={
-//                 <Trans
-//             i18nKey="buttons.colorModeSwitch"/>
-//             }
-//             icon={
-//                 colorMode === colorModes.dark ? (<Moon height='24px' width='24px' color="white"/>) : (<Moon height='24px' width='24px' color="black"/>)
-//             }
-//             block={true}
-//             onClick={
-//                 () => {
-//                     colorModeToggle.toggleColorMode()
-//                     onClick ?. ()
-//                 }
-//             }
-//             buttonType={
-//                 colorMode === colorModes.dark ? buttonTypes.white : buttonTypes.black
-//             }/>);
-//         // eslint-disable-next-line react-hooks/exhaustive-deps
-//     }, [colorMode])
-// }
-
-    //
-    // <button className="btn btn-sm navbar-btn theme-switch-button" aria-label="light/dark" data-bs-placement="bottom"
-    //         data-bs-toggle="tooltip" data-bs-original-title="light/dark">
-    //     <span className="theme-switch nav-link" data-mode="light"><svg className="svg-inline--fa fa-sun fa-lg"
-    //                                                                    aria-hidden="true" focusable="false"
-    //                                                                    data-prefix="fas" data-icon="sun" role="img"
-    //                                                                    xmlns="http://www.w3.org/2000/svg"
-    //                                                                    viewBox="0 0 512 512" data-fa-i2svg=""><path
-    //         fill="currentColor"
-    //         d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"></path></svg>
-    //         <!-- <i class="fa-solid fa-sun fa-lg"></i> Font Awesome fontawesome.com --></span>
-    //     <span className="theme-switch nav-link" data-mode="dark"><svg className="svg-inline--fa fa-moon fa-lg"
-    //                                                                   aria-hidden="true" focusable="false"
-    //                                                                   data-prefix="fas" data-icon="moon" role="img"
-    //                                                                   xmlns="http://www.w3.org/2000/svg"
-    //                                                                   viewBox="0 0 384 512" data-fa-i2svg=""><path
-    //         fill="currentColor"
-    //         d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path></svg>
-    //         <!-- <i class="fa-solid fa-moon fa-lg"></i> Font Awesome fontawesome.com --></span>
-    //     <span className="theme-switch nav-link" data-mode="auto"><svg
-    //         className="svg-inline--fa fa-circle-half-stroke fa-lg" aria-hidden="true" focusable="false"
-    //         data-prefix="fas" data-icon="circle-half-stroke" role="img" xmlns="http://www.w3.org/2000/svg"
-    //         viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor"
-    //                                                      d="M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"></path></svg>
-    //         <!-- <i class="fa-solid fa-circle-half-stroke fa-lg"></i> Font Awesome fontawesome.com --></span>
-    // </button>
-    //
-    // <div className="bd-nav-themes">
-    //     <button id="js-cycle" className="bd-cycle js-burger is-sun" data-target="js-themes">
-    //         <div className="bd-cycles">
-    //             <div className="bd-cycle-sun">
-    //       <span className="icon">
-    //         <i className="fas fa-lg fa-sun" aria-hidden="true"></i>
-    //       </span>
-    //             </div>
-    //
-    //             <div className="bd-cycle-moon">
-    //       <span className="icon">
-    //         <i className="fas fa-lg fa-moon" aria-hidden="true"></i>
-    //       </span>
-    //             </div>
-    //         </div>
-    //     </button>
-    //
-    //     <div id="js-themes" className="bd-nav-menu is-cycles js-menu js-themes">
-    //         <button data-scheme="light" className="bd-nav-item is-sun is-active" aria-label="Light mode">
-    //     <span className="icon">
-    //       <i className="fas fa-sun" aria-hidden="true"></i>
-    //     </span>
-    //             <span>Light</span>
-    //         </button>
-    //
-    //         <button data-scheme="dark" className="bd-nav-item is-moon" aria-label="Dark mode">
-    //     <span className="icon">
-    //       <i className="fas fa-moon" aria-hidden="true"></i>
-    //     </span>
-    //             <span>Dark</span>
-    //         </button>
-    //
-    //         <button data-scheme="system" className="bd-nav-item is-system" aria-label="System mode">
-    //     <span className="icon">
-    //       <i className="fas fa-desktop" aria-hidden="true"></i>
-    //     </span>
-    //             <span>System</span>
-    //         </button>
-    //     </div>
-    // </div>
-
 
 
 
@@ -254,9 +114,11 @@
                 },
                 html: `
                     <div class="">
-                        <button class="button without-border">
+                        <button id="theme-change" class="button without-border">
                             <span style="width:1.5em;height:1.5em">
-                                <i class="fas fa-lg fa-sun"></i>
+                                <svg class="theme-system" style="display:none;" fill="#3C9D72" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg>
+                                <svg class="theme-dark" style="display:none;" fill="#8156F5" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
+                                <svg class="theme-light" style="display:flex;" fill="#FFB70F" viewBox="0 0 512 512"><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>
                             </span>
                         </button>
                     </div>
@@ -312,56 +174,6 @@
         /*---------------------------------------*
          *      Set initial state
          *---------------------------------------*/
-    }
-
-    /************************************************************
-     *  Color luna: #8156F5
-     *  Color sol: #FFB70F
-     ************************************************************/
-    function html_change_theme(self)
-    {
-        return `
-        <div class="bd-nav-themes">
-            <button id="js-cycle" class="bd-cycle js-burger is-moon" data-target="js-themes">
-              <div class="bd-cycles">
-                <div class="bd-cycle-sun">
-                  <span class="icon">
-                    <i class="fas fa-lg fa-sun" aria-hidden="true"></i>
-                  </span>
-                </div>
-
-                <div class="bd-cycle-moon">
-                  <span class="icon">
-                    <i class="fas fa-lg fa-moon" aria-hidden="true"></i>
-                  </span>
-                </div>
-              </div>
-            </button>
-
-            <div id="js-themes" class="bd-nav-menu is-cycles js-menu js-themes">
-              <button data-scheme="light" class="bd-nav-item is-sun" aria-label="Light mode">
-                <span class="icon">
-                  <i class="fas fa-sun" aria-hidden="true"></i>
-                </span>
-                <span>Light</span>
-              </button>
-
-              <button data-scheme="dark" class="bd-nav-item is-moon is-active" aria-label="Dark mode">
-                <span class="icon">
-                  <i class="fas fa-moon" aria-hidden="true"></i>
-                </span>
-                <span>Dark</span>
-              </button>
-
-              <button data-scheme="system" class="bd-nav-item is-system" aria-label="System mode">
-                <span class="icon">
-                  <i class="fas fa-desktop" aria-hidden="true"></i>
-                </span>
-                <span>System</span>
-              </button>
-            </div>
-          </div>
-        `;
     }
 
 
@@ -570,9 +382,42 @@
      ************************************************/
     function ac_change_theme(self, event, kw, src)
     {
-        let theme = 'dark';
-        document.documentElement.setAttribute("data-theme", theme);
-        localStorage.setItem('theme', theme); // Save theme preference
+        let current_theme = localStorage.getItem('theme') || 'light';
+
+        let $svg = jQuery("#theme-change svg");
+        $svg.css('display', 'none');
+
+        let new_theme;
+        switch(current_theme) {
+            case 'dark':
+                new_theme = 'system';
+                break;
+            case 'system':
+                new_theme = 'light';
+                break;
+            case 'light':
+            default:
+                new_theme = 'dark';
+                break;
+        }
+
+        switch(new_theme) {
+            case 'dark':
+                $svg = jQuery("#theme-change svg.theme-dark");
+                break;
+            case 'system':
+                $svg = jQuery("#theme-change svg.theme-system");
+                break;
+            case 'light':
+            default:
+                $svg = jQuery("#theme-change svg.theme-light");
+                break;
+        }
+        document.documentElement.setAttribute("data-theme", new_theme);
+        $svg.css('display', 'flex');
+
+        localStorage.setItem('theme', new_theme); // Save theme preference
+
         return 0;
     }
 
